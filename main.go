424d71d0e1835afbf939848155078b9aa0867323
wahyusa <27491616+wahyusa@users.noreply.github.com>
2025-10-24 13:25:30 +0700

    fix serve something with net http instead of just text LOL

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
+// main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hi Mom!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // default port
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "✅ Hello, world! This Go app is running on port %s\n", port)
+	})
+
+	log.Printf("🚀 Server running on http://localhost:%s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
